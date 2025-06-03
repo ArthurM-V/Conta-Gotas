f7851e99b41f08aec03570aec10ae381d327a3ae
Arthur Mesquita <arhurmesquita404@gmail.com>
2025-06-03 17:04:59 -0300

    basic version

---
+++ hints.js
@@ -0,0 +1,32 @@
+"use strict";
+
+export const dicas = new Map([
+  [
+    1,
+    "Antes mesmo do café da manhã, beba um copo de água para ativar o metabolismo e repor os líquidos perdidos durante o sono.",
+  ],
+  [
+    2,
+    "Tenha sempre uma garrafa por perto, seja no trabalho, na mochila ou no carro. Isso facilita o acesso e incentiva o consumo.",
+  ],
+  [
+    3,
+    "Se tiver dificuldade para beber água pura, adicione fatias de limão, laranja, hortelã ou pepino para dar sabor sem calorias.",
+  ],
+  [
+    4,
+    "Inclua frutas e vegetais como melancia, pepino, morango, laranja e alface na sua dieta, eles ajudam a manter o corpo hidratado.",
+  ],
+  [
+    5,
+    "Dietas ricas em sal aumentam a retenção de líquidos e a sensação de sede. Modere para manter o equilíbrio hídrico do corpo.",
+  ],
+  [
+    6,
+    "Garrafas com indicadores de horas ou mililitros ajudam a controlar o quanto já foi bebido e quanto ainda falta.",
+  ],
+  [
+    7,
+    "Embora beber água seja o principal, o uso de hidratantes ajuda a manter a pele saudável, especialmente em climas secos.",
+  ],
+]);
